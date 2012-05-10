864af035c4c0210e58dcd54ea09d9c73e4bdd0eb
Bruno Basto <bruno.basto@liferay.com>
2012-05-10 19:49:37 -0300

    LPS-26069 Differentiate all day events from regular events

--- portlets/enterprise-calendar-portlet/docroot/js/javascript.js
+++ portlets/enterprise-calendar-portlet/docroot/js/javascript.js
@@ -671,6 +671,28 @@
 					return (instance.get('parentCalendarBookingId') === instance.get('calendarBookingId'));
 				},
 
+				syncNodeColorUI: function() {
+					var instance = this;
+
+					Liferay.SchedulerEvent.superclass.syncNodeColorUI.apply(instance, arguments);
+
+					var node = instance.get('node');
+					var scheduler = instance.get('scheduler');
+
+					if (scheduler) {
+						var activeViewName = scheduler.get('activeView').get('name');
+
+						if ((activeViewName === 'month') && !instance.get('allDay')) {
+							node.setStyles(
+								{
+									backgroundColor: 'transparent',
+									border: 'none'
+								}
+							);
+						}
+					}
+				},
+
 				_onLoadingChange: function(event) {
 					var instance = this;
 
